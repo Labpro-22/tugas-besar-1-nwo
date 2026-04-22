23673db0622cb4b574c3a201922fa8c4f9eecb6f
fariswirakusuma <fariswkt@gmail.com>
2026-04-22 10:30:42 +0700

    feat: add GameGUI.hpp

---
+++ include/views/GameGUI.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <bits/stdc++.h>
+class GameManager;
+class PropertyTile;
+class TilePos;
+class Tile;
+
+class GameGUI {
+private:
+    GameManager& gm;
+    const int screenWidth = 1200;
+    const int screenHeight = 1000;
+    const int tileSize = 80;
+    const int boardOffset = 50;
+    PropertyTile* activeDeed = nullptr; 
+    bool showDeedPopup = false;
+    bool showSkillMenu = false;
+    bool showAssetList = false; 
+    struct TilePos { int x, y; };
+    std::map<int, TilePos> tileMap;
+
+    void initTileMap();
+    void drawTile(int index, Tile& tile);
+    void drawPlayers();
+    void drawUI(); 
+    void drawLogBox();
+    void drawPlayerHUD();
+    
+    void drawAssetList();
+    void drawSkillMenu();
+    void drawPropertyDeed(); 
+
+public:
+    GameGUI(GameManager& manager);
+    
+    void openDeed(PropertyTile* prop) { 
+        if (prop != nullptr) {
+            activeDeed = prop; 
+            showDeedPopup = true; 
+            showAssetList = false;
+        }
+    }
+    void closeDeed() { activeDeed = nullptr; showDeedPopup = false; }
+    bool isDeedOpen() const { return showDeedPopup; }
+
+    void toggleSkillMenu() { 
+        showSkillMenu = !showSkillMenu; 
+        if(showSkillMenu) { showAssetList = false; showDeedPopup = false; }
+    }
+    void closeSkillMenu() { showSkillMenu = false; }
+    bool isSkillMenuOpen() const { return showSkillMenu; }
+
+    void toggleAssetList() { 
+        showAssetList = !showAssetList; 
+        if(showAssetList) { showSkillMenu = false; showDeedPopup = false; }
+    }
+    void closeAssetList() { showAssetList = false; }
+    bool isAssetListOpen() const { return showAssetList; }
+    
+    bool isAnyMenuOpen() const { 
+        return showSkillMenu || showDeedPopup || showAssetList; 
+    }
+};
--- include/views/views.hpp
+++ include/views/views.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-#include "GameCLI.hpp"
+#include "GameGUI.hpp"
