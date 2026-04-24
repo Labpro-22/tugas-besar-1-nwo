a3e82d02d6792937e40f1c25e7e47c78ca3cb260
fariswirakusuma <fariswkt@gmail.com>
2026-04-24 19:19:24 +0700

    feat: add data testcases

--- src/core/StatePromptUpgrade.cpp
+++ src/core/StatePromptUpgrade.cpp
@@ -37,20 +37,20 @@ void StatePromptUpgrade::handleInput(GameManager& gm, GameGUI& gui) {
 void StatePromptUpgrade::updateUI(GameManager& gm, GameGUI& gui) {
     if (gui.isAnyMenuOpen()) return;
     
-    std::cout << "[DEBUG] 1. Masuk updateUI Upgrade..." << std::endl;
+    // std::cout << "[DEBUG] 1. Masuk updateUI Upgrade..." << std::endl;
     Player& p = gm.getCurrentPlayer();
     
-    std::cout << "[DEBUG] 2. Ngambil posisi player: " << p.getPosition() << std::endl;
+    // std::cout << "[DEBUG] 2. Ngambil posisi player: " << p.getPosition() << std::endl;
     
     PropertyTile* prop = gm.getBoard().getTile(p.getPosition()).getAsProperty();
-    std::cout << "[DEBUG] 3. Pointer PropertyTile aman..." << std::endl;
+    // std::cout << "[DEBUG] 3. Pointer PropertyTile aman..." << std::endl;
     
     if (prop) {
-        std::cout << "[DEBUG] 4. Cek canBuild()..." << std::endl;
+        // std::cout << "[DEBUG] 4. Cek canBuild()..." << std::endl;
         if (prop->canBuild()) {
             
-            std::cout << "[DEBUG] 5. Ambil Nama Properti: " << prop->getName() << std::endl;
-            std::cout << "[DEBUG] 6. Ambil Harga Rumah: " << prop->getHousePrice() << std::endl;
+            // std::cout << "[DEBUG] 5. Ambil Nama Properti: " << prop->getName() << std::endl;
+            // std::cout << "[DEBUG] 6. Ambil Harga Rumah: " << prop->getHousePrice() << std::endl;
             
             DrawRectangle(200, 300, 400, 200, Fade(BLACK, 0.9f));
             DrawRectangleLines(200, 300, 400, 200, GREEN);
@@ -62,7 +62,7 @@ void StatePromptUpgrade::updateUI(GameManager& gm, GameGUI& gui) {
             DrawText("[1] Bangun Rumah", 230, 410, 20, LIME);
             DrawText("[2] Lewati", 230, 440, 20, RED);
             
-            std::cout << "[DEBUG] 7. Selesai ngegambar UI!" << std::endl;
+            // std::cout << "[DEBUG] 7. Selesai ngegambar UI!" << std::endl;
         }
     }
 }
